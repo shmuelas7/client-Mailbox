a7328e23c4116c56e776bf7bdf4cad83e84c85ef
sagi buta <sagibuta@gmail.com>
2024-03-27 13:44:20 +0300

    creat inbox copm

---
+++ src/components/Inbox/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from './style.module.css'
+
+export default function index() {
+ 
+ 
+ 
+ 
+ 
+    return (
+    <div className={styles.Inbox}>
+        content
+
+    </div>
+  )
+}
